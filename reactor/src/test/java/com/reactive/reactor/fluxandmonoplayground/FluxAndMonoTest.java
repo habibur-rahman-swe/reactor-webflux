d856031881010710b8d8efcf61da8739fac86fc1
Habibur Rahman - TIL <habibur0199mhr@gmail.com>
2026-09-17 17:51:46 +0600

    Refactor FluxAndMonoTest to improve readability and structure of test methods

--- reactor/src/test/java/com/reactive/reactor/fluxandmonoplayground/FluxAndMonoTest.java
+++ reactor/src/test/java/com/reactive/reactor/fluxandmonoplayground/FluxAndMonoTest.java
@@ -2,11 +2,22 @@ package com.reactive.reactor.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 
+import reactor.core.publisher.Flux;
+
 public class FluxAndMonoTest {
 
 	@Test
 	public void fluxText() {
-		
+		Flux<String> stringFlux = Flux.just("A", "B", "C")
+//				 .concatWith(Flux.error(new RuntimeException("Error!")))v
+				.concatWith(Flux.just("After Error")).log();
+
+		stringFlux.subscribe(System.out::println, (e) -> System.err.println(e), () -> System.out.println("Completed"));
 	}
-	
+
+	public void fluxTestElement_withoutError() {
+		Flux<String> stringFlux = Flux.just("A", "B", "C")
+				.log();
+
+	}
 }
